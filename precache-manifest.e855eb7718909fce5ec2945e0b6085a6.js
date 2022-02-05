5d2bf9ecc5a1193b7cf23ff0529bdab0caa175a8
namannn <46934223+namannn@users.noreply.github.com>
2022-02-05 19:12:36 +0530

    Updates

---
+++ precache-manifest.e855eb7718909fce5ec2945e0b6085a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94893999b135fe6db7d0f2ec412073b",
+    "url": "/NewsWala/index.html"
+  },
+  {
+    "revision": "c180be81ad175a49a3cb",
+    "url": "/NewsWala/static/css/main.0deb154f.chunk.css"
+  },
+  {
+    "revision": "5120289899e9d38dac20",
+    "url": "/NewsWala/static/js/2.116cf1fa.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/NewsWala/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "c180be81ad175a49a3cb",
+    "url": "/NewsWala/static/js/main.22d01ddf.chunk.js"
+  },
+  {
+    "revision": "ca54c70715c42ee9755b",
+    "url": "/NewsWala/static/js/runtime~main.cc5abaf7.js"
+  },
+  {
+    "revision": "c015e4eb0b0e9ee6c68b0d21aea448b8",
+    "url": "/NewsWala/static/media/spinner.c015e4eb.gif"
+  }
+]);
